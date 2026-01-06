04e32904b7cdbb795264d2a3a650481233977a8c
ben.grob <ben.grob@grobis.ch>
2026-01-06 23:14:47 +0100

    changed browserrouter to hashrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import './App.css'
 
 // Import Components
